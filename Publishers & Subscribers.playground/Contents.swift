2853a6cfea7541fd17bff8a31ba050c8354762e4
Olexsii <realcarter50@gmail.com>
2022-10-19 22:38:11 +0300

    assing(to:on:)

--- Publishers & Subscribers.playground/Contents.swift
+++ Publishers & Subscribers.playground/Contents.swift
@@ -14,6 +14,58 @@ example(of: "Publisher") {
         print("Notification received!")
     }
     
+    let subscription = publisher
+        .sink { _ in
+            print("Notification recived from a publisher!")
+        }
+    
     center.post(name: myNotification, object: nil)
+    
+    subscription.cancel()
     center.removeObserver(observer)
+}
+
+
+example(of: "Subscriber") {
+    let myNotification = Notification.Name("MyNotification")
+    let center = NotificationCenter.default
+    
+    let publisher = center.publisher(for: myNotification)
+}
+
+
+example(of: "Just") {
+    let just = Just("Hello World!")
+    
+    _ = just
+        .sink(receiveCompletion: {
+            print("Received completion", $0)
+        }, receiveValue: {
+            print("Received value", $0)
+        })
+   
+    _ = just
+        .sink(receiveCompletion: {
+            print("Received completion (another)", $0)
+        }, receiveValue: {
+            print("Received value (another)", $0)
+        })
+}
+
+
+example(of: "assing(to:on:)") {
+    
+    class SomeObject {
+        var value: String = "" {
+            didSet {
+                print(value)
+            }
+        }
+    }
+    
+    let object = SomeObject()
+    let publisher = ["Hello", "World!"].publisher
+    
+    _ = publisher
+        .assign(to: \.value, on: object)
 }
