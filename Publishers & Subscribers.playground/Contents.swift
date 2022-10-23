00258ae102ddcfdb596063567f5ceea2ec3d0b0f
Olexsii <realcarter50@gmail.com>
2022-10-23 21:06:11 +0300

    Future

--- Publishers & Subscribers.playground/Contents.swift
+++ Publishers & Subscribers.playground/Contents.swift
@@ -113,3 +113,32 @@ example(of: "Custom Subscription") {
     let subscriber = InSubscriber()
     publisher.subscribe(subscriber)
 }
+
+
+//example(of: "Future") {
+//    func futureIncrement(integer: Int, afterDelay delay: TimeInterval) -> Future<Int, Never> {
+//        Future<Int, Never> { promise in
+//            print("Original")
+//            DispatchQueue.global().asyncAfter(deadline: .now() + delay) {
+//                promise(.success(integer + 1))
+//            }
+//        }
+//    }
+//
+//    let future = futureIncrement(integer: 1, afterDelay: 3)
+//    future
+//        .sink {
+//            print($0)
+//        } receiveValue: {
+//            print($0)
+//        }
+//        .store(in: &subscriptions)
+//
+//    future
+//        .sink(receiveCompletion: {
+//            print("Second", $0)
+//        }, receiveValue: {
+//            print("Second", $0)
+//        })
+//        .store(in: &subscriptions)
+//}
