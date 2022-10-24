2952260fd18094c85cbcdfe13ce36b0861867eb1
Olexsii <realcarter50@gmail.com>
2022-10-24 15:19:18 +0300

    Dynamically adjusting demand

--- Publishers & Subscribers.playground/Contents.swift
+++ Publishers & Subscribers.playground/Contents.swift
@@ -213,3 +213,45 @@ example(of: "CurrentValueSubject") {
         .store(in: &subcriptions)
     subject.send(completion: .finished)
 }
+
+
+example(of: "Dynamically adjusting Demand") {
+    
+    final class IntSubcriber: Subscriber {
+        typealias Input = Int
+        typealias Failure = Never
+        
+        func receive(subscription: Subscription) {
+            subscription.request(.max(2))
+        }
+        
+        func receive(_ input: Int) -> Subscribers.Demand {
+            print("Received value", input)
+            
+            switch input {
+            case 1:
+                return .max(2)
+            case 3:
+                return .max(1   )
+            default:
+                return .none
+            }
+        }
+        
+        func receive(completion: Subscribers.Completion<Never>) {
+            print("Received completion", completion)
+        }
+    }
+    
+    let subscriber = IntSubcriber()
+    
+    let subject = PassthroughSubject<Int, Never>()
+    subject.subscribe(subscriber)
+    
+    subject.send(1)
+    subject.send(2)
+    subject.send(3)
+    subject.send(4)
+    subject.send(5)
+    subject.send(6)
+}
