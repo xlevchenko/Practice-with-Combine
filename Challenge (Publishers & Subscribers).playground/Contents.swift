76e6298fcf7444c1bdb9ab3e02d37e1756b506e8
Olexsii <realcarter50@gmail.com>
2022-10-24 21:50:17 +0300

    Challenge: Create a Blackjack card dealer

---
+++ Challenge (Publishers & Subscribers).playground/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+import Combine
+
+var subscriptions = Set<AnyCancellable>()
+
+example(of: "Create a Blackjack card dealer") {
+  let dealtHand = PassthroughSubject<Hand, HandError>()
+
+  
+  func deal(_ cardCount: UInt) {
+    var deck = cards
+    var cardsRemaining = 52
+    var hand = Hand()
+    
+    for _ in 0 ..< cardCount {
+      let randomIndex = Int.random(in: 0 ..< cardsRemaining)
+      hand.append(deck[randomIndex])
+      deck.remove(at: randomIndex)
+      cardsRemaining -= 1
+    }
+      
+    // Add code to update dealtHand here
+      if hand.points > 21 {
+          dealtHand.send(completion: .failure(.busted))
+      } else {
+          dealtHand.send(hand)
+      }
+  }
+  
+  // Add subscription to dealtHand here
+    _ = dealtHand
+        .sink(receiveCompletion: {
+            if case let .failure(error) = $0 {
+                print(error)
+            }
+        }, receiveValue: { hand in
+            print(hand.cardString, "for", hand.points, "points")
+        })
+  
+  deal(3)
+}
