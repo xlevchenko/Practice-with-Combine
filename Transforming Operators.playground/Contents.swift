75f181ecced44c85c3bcd65d3ed93a58b2a94a4c
Olexsii <realcarter50@gmail.com>
2022-10-25 20:48:32 +0300

    Operator collect

---
+++ Transforming Operators.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+import Combine
+
+var subscriptions = Set<AnyCancellable>()
+
+example(of: "collect") {
+    ["A", "B", "C", "D", "E"].publisher
+        .collect(2)
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
