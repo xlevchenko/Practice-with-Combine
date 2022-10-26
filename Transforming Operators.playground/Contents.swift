0c0d3d6a4e6415cdbfd15fe36759a193a7c4102b
Olexsii <realcarter50@gmail.com>
2022-10-26 23:45:03 +0300

    flatMap

--- Transforming Operators.playground/Contents.swift
+++ Transforming Operators.playground/Contents.swift
@@ -46,3 +46,24 @@ example(of: "tryMap") {
         .sink(receiveCompletion: { print($0) }, receiveValue: {print($0) })
         .store(in: &subscriptions)
 }
+
+
+example(of: "flatMap") {
+    func decode(_ codes: [Int]) -> AnyPublisher<String, Never> {
+        Just(
+            codes
+                .compactMap({ code in
+                    guard (32...255).contains(codes) else { return nil }
+                    return String(UnicodeScalar(code) ?? " ")
+                })
+                .joined()
+        )
+        .eraseToAnyPublisher()
+    }
+    
+    [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33].publisher
+        .collect()
+        .flatMap(decode)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
