70b32dce3415d17a546f58aaa02e83ebd18066b9
Olexsii <realcarter50@gmail.com>
2022-11-05 23:06:59 +0200

    prepend(Publisher)

--- Combining Operators.playground/Contents.swift
+++ Combining Operators.playground/Contents.swift
@@ -24,3 +24,29 @@ example(of: "prepend(Sequence)") {
         .sink(receiveValue: { print($0) })
         .store(in: &subscriptions)
 }
+
+
+example(of: "prepend(Publisher)") {
+    let publisher1 = [3, 4].publisher
+    let publisher2 = [1, 2].publisher
+    
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+
+example(of: "prepend(Publisher) #2") {
+    let publisher1 = [3, 4].publisher
+    let publisher2 = PassthroughSubject<Int, Never>()
+    
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    publisher2.send(1)
+    publisher2.send(2)
+    publisher2.send(completion: .finished)
+}
