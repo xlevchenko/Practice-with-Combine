d7139800ea890b1563521d190d646e4c20f4be1b
Olexsii <realcarter50@gmail.com>
2022-11-05 22:25:29 +0200

    prepend(Sequence)

--- Combining Operators.playground/Contents.swift
+++ Combining Operators.playground/Contents.swift
@@ -12,3 +12,15 @@ example(of: "prepend(Output...)") {
         .sink(receiveValue: { print($0) })
         .store(in: &subscriptions)
 }
+
+
+example(of: "prepend(Sequence)") {
+    let publisher = [5, 6, 7].publisher
+    
+    publisher
+        .prepend([3, 4])
+        .prepend(stride(from: 6, to: 11, by: 2))
+        .prepend(Set(1...2))
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
