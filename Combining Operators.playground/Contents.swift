0a6bed95c9f992b64dae30d19cc5340a1db2f783
Olexsii <realcarter50@gmail.com>
2022-11-06 17:33:10 +0200

    combineLatest

--- Combining Operators.playground/Contents.swift
+++ Combining Operators.playground/Contents.swift
@@ -217,3 +217,30 @@ example(of: "combineLatest") {
     publisher1.send(completion: .finished)
     publisher2.send(completion: .finished)
 }
+
+
+example(of: ".zip") {
+    let publisher1 = PassthroughSubject<Int, Never>()
+    let publisher2 = PassthroughSubject<String, Never>()
+    
+    publisher1
+        .zip(publisher2)
+        .sink { _ in
+            print("Completed")
+        } receiveValue: { print("P1: \($0), P2: \($1)")}
+        .store(in: &subscriptions)
+    
+    publisher1.send(1)
+    publisher1.send(2)
+    
+    publisher2.send("a")
+    publisher2.send("b")
+    
+    publisher1.send(3)
+    publisher2.send("c")
+    publisher2.send("d")
+    
+    publisher1.send(completion: .finished)
+    publisher2.send(completion: .finished)
+}
+
