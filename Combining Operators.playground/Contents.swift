59cd5de1dcbd835aa7e8e4eff1b271d0f3621bb9
Olexsii <realcarter50@gmail.com>
2022-11-06 15:13:27 +0200

    switchTpLatest

--- Combining Operators.playground/Contents.swift
+++ Combining Operators.playground/Contents.swift
@@ -99,3 +99,36 @@ example(of: "append(Publisher)") {
         .sink(receiveValue: { print($0) })
         .store(in: &subscriptions)
 }
+
+
+example(of: "switchToLatest") {
+    let publisher1 = PassthroughSubject<Int, Never>()
+    let publisher2 = PassthroughSubject<Int, Never>()
+    let publisher3 = PassthroughSubject<Int, Never>()
+    
+    let publisher = PassthroughSubject<PassthroughSubject<Int, Never>, Never>()
+    
+    publisher
+        .switchToLatest()
+        .sink(receiveCompletion: { _ in print("Completed") },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    publisher.send(publisher1)
+    publisher1.send(1)
+    publisher1.send(2)
+    
+    publisher.send(publisher2)
+    publisher1.send(3)
+    publisher2.send(4)
+    publisher2.send(5)
+    
+    publisher.send(publisher3)
+    publisher2.send(6)
+    publisher3.send(7)
+    publisher3.send(8)
+    publisher3.send(9)
+    
+    publisher3.send(completion: .finished)
+    publisher.send(completion: .finished)
+}
