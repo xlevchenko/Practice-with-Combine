4e745c2e5d31530c135422d8e9c37c2b69a2d9fd
Olexsii <realcarter50@gmail.com>
2022-11-13 22:09:24 +0200

    Collecting values (part 2)

--- Time Manipulation Operators.playground/Pages/Collect.xcplaygroundpage/Contents.swift
+++ Time Manipulation Operators.playground/Pages/Collect.xcplaygroundpage/Contents.swift
@@ -4,11 +4,16 @@ import PlaygroundSupport
 
 let valuePerSecond = 1.0
 let collectTimeStride = 4
+let collectedMaxCount = 2
 
 let sourcePublisher = PassthroughSubject<Date, Never>()
 
 let collectedPublisher = sourcePublisher
     .collect(.byTime(DispatchQueue.main, .seconds(collectTimeStride)))
+    .flatMap { dates in dates.publisher }
+
+let collectedPublisher2 = sourcePublisher
+    .collect(.byTimeOrCount(DispatchQueue.main, .seconds(collectTimeStride), collectedMaxCount))
     .flatMap { dates in dates.publisher }
 
 let subscription = Timer
@@ -18,13 +23,16 @@ let subscription = Timer
 
 let sourceTimeline = TimelineView(title: "Emitted values:")
 let collectedTimeline = TimelineView(title: "Collected values (every \(collectTimeStride)s):")
+let collectedTimeline2 = TimelineView(title: "Collected values (at most \(collectedMaxCount) every \(collectTimeStride)s):")
 
 let view = VStack(spacing: 40) {
     sourceTimeline
     collectedTimeline
+    collectedTimeline2
 }
 
 PlaygroundPage.current.liveView = UIHostingController(rootView: view.frame(width: 375, height: 600))
 
 sourcePublisher.displayEvents(in: sourceTimeline)
 collectedPublisher.displayEvents(in: collectedTimeline)
+collectedPublisher2.displayEvents(in: collectedTimeline2)
