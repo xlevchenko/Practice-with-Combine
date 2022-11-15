9fb5b17f7bc8a8ef9285ea5146ca148dc096083f
Olexsii <realcarter50@gmail.com>
2022-11-15 19:45:40 +0200

    Challenge completed

---
+++ Challenge(Time manipulation operators).playground/Contents.swift
@@ -0,0 +1,7 @@
+import Combine
+import Foundation
+
+// A subject you get values from
+let subject = PassthroughSubject<Int, Never>()
+
+<#Add your code here#>
--- Time Manipulation Operators.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
+++ Time Manipulation Operators.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
@@ -2,35 +2,34 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
-<# Add your code here #>
+let subject = PassthroughSubject<String, Never>()
 
-/*:
- Copyright (c) 2021 Razeware LLC
+let measureSubject = subject.measureInterval(using: DispatchQueue.main)
+let measureSubject2 = subject.measureInterval(using: RunLoop.main)
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+let subjectTimeline = TimelineView(title: "Emitted values")
+let measureTimeline = TimelineView(title: "Measured values")
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+let view = VStack(spacing: 100) {
+    subjectTimeline
+    measureTimeline
+}
 
- Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- distribute, sublicense, create a derivative work, and/or sell copies of the
- Software in any work that is designed, intended, or marketed for pedagogical or
- instructional purposes related to programming, coding, application development,
- or information technology.  Permission for such use, copying, modification,
- merger, publication, distribution, sublicensing, creation of derivative works,
- or sale is expressly withheld.
+PlaygroundPage.current.liveView = UIHostingController(rootView: view.frame(width: 375, height: 600))
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+subject.displayEvents(in: subjectTimeline)
+measureSubject.displayEvents(in: measureTimeline)
 
+let subscription1 = subject.sink { value in
+    print("+\(deltaTime)s: Subject emitted: \(value)")
+}
+
+let subscription2 = measureSubject.sink { value in
+    print("+\(deltaTime)s: Measure emitted: \(Double(value.magnitude) / 1_000_000_000.0)")
+}
+
+let subscription3 = measureSubject.sink { value in
+    print("+\(deltaTime)s: Measure2 emitted: \(value)")
+}
+
+subject.feed(with: typingHelloWorld)
