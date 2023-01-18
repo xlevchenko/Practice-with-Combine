5380ee6e96e000aa45d04c23dcfff3c776195a27
Olexsii <realcarter50@gmail.com>
2023-01-18 22:12:47 +0200

    Chapter 13: Resource Management

---
+++ Resource Management.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+import Combine
+
+let shared = URLSession.shared
+    .dataTaskPublisher(for: URL(string: "https://www.raywenderlich.com")!)
+    .map(\.data)
+    .print("shared")
+    .share()
+
+print("subscribing first")
+
+let subscription1 = shared.sink { _ in
+} receiveValue: {
+    print("subscription1 received: \($0)")
+}
+
+print("subscribing second")
+
+//let subscription2 = shared.sink { _ in
+//} receiveValue: {
+//    print("subscription2 received: \($0)")
+//}
+
+var subscription2: AnyCancellable? = nil
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    print("subscribing second")
+    
+    subscription2 = shared.sink(
+        receiveCompletion: { print("subscription2 completion \($0)")},
+        receiveValue: { print("subcription2 received: \($0)") }
+    )
+}
