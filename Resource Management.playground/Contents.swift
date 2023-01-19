983d9796305547448cbf1057af70174712ef9655
Olexsii <realcarter50@gmail.com>
2023-01-19 22:42:24 +0200

    multicast(_:) operator

--- Resource Management.playground/Contents.swift
+++ Resource Management.playground/Contents.swift
@@ -1,33 +1,50 @@
 import UIKit
 import Combine
 
-let shared = URLSession.shared
-    .dataTaskPublisher(for: URL(string: "https://www.raywenderlich.com")!)
-    .map(\.data)
-    .print("shared")
-    .share()
-
-print("subscribing first")
-
-let subscription1 = shared.sink { _ in
-} receiveValue: {
-    print("subscription1 received: \($0)")
-}
-
-print("subscribing second")
+//let shared = URLSession.shared
+//    .dataTaskPublisher(for: URL(string: "https://www.raywenderlich.com")!)
+//    .map(\.data)
+//    .print("shared")
+//    .share()
+//
+//print("subscribing first")
+//
+//let subscription1 = shared.sink { _ in
+//} receiveValue: {
+//    print("subscription1 received: \($0)")
+//}
+//
+//print("subscribing second")
 
 //let subscription2 = shared.sink { _ in
 //} receiveValue: {
 //    print("subscription2 received: \($0)")
 //}
 
-var subscription2: AnyCancellable? = nil
+//var subscription2: AnyCancellable? = nil
+//
+//DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//    print("subscribing second")
+//    
+//    subscription2 = shared.sink(
+//        receiveCompletion: { print("subscription2 completion \($0)")},
+//        receiveValue: { print("subcription2 received: \($0)") }
+//    )
+//}
 
-DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-    print("subscribing second")
-    
-    subscription2 = shared.sink(
-        receiveCompletion: { print("subscription2 completion \($0)")},
-        receiveValue: { print("subcription2 received: \($0)") }
-    )
-}
+
+let subject = PassthroughSubject<Data, URLError>()
+
+let multicasted = URLSession.shared
+    .dataTaskPublisher(for: URL(string: "https://www.raywenderlich.com")!)
+    .map(\.data)
+    .print("multicast")
+    .multicast(subject: subject)
+
+let subscription3 = multicasted
+    .sink { _ in }
+receiveValue: {
+        print("subscription3 received: '\($0)'")
+    }
+
+let cancellable = multicasted.connect()
