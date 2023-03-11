292d08afbd6fc7f82789cda00ade7e5b3c416fee
Olexsii <realcarter50@gmail.com>
2023-03-11 22:03:50 +0200

    Handler error using network api layer

--- Error Handling.playground/Pages/Designing your fallible APIs.xcplaygroundpage/Contents.swift
+++ Error Handling.playground/Pages/Designing your fallible APIs.xcplaygroundpage/Contents.swift
@@ -4,7 +4,80 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 //: ## Designing your fallible APIs
-<#Add your code here#>
+example(of: "Joke API") {
+    class DadJokes {
+        
+        struct Joke: Codable {
+            let id: String
+            let joke: String
+        }
+        
+        enum Error: Swift.Error, CustomStringConvertible {
+            
+            case network
+            case jokeDoesntExist(id: String)
+            case parsing
+            case unknown
+            
+            var description: String {
+                switch self {
+                case .network:
+                    return "Request to API Server failed"
+                case .jokeDoesntExist(id: let id):
+                    return "Joke with ID \(id) doesn't exist"
+                case .parsing:
+                    return "Failed pasing response from server"
+                case .unknown:
+                    return "An unknown error occurred"
+                }
+            }
+        }
+        
+        func getJoke(id: String) -> AnyPublisher<Joke, Error> {
+            guard id.rangeOfCharacter(from: .letters) != nil else {
+                return Fail<Joke, Error>(error: .jokeDoesntExist(id: id))
+                    .eraseToAnyPublisher()
+            }
+            
+            let url = URL(string: "https://icanhazdadjoke.com/j/\(id)")!
+            var request = URLRequest(url: url)
+            request.allHTTPHeaderFields = ["Accept": "application/json"]
+            
+            return URLSession.shared
+                .dataTaskPublisher(for: request)
+                .tryMap({ data, _ -> Data in
+                    guard let obj = try? JSONSerialization.jsonObject(with: data),
+                          let dict = obj as? [String: Any],
+                          dict["status"] as? Int == 404 else {
+                        return data
+                    }
+                    throw DadJokes.Error.jokeDoesntExist(id: id)
+                })
+                .decode(type: Joke.self, decoder: JSONDecoder())
+                .mapError({ error -> DadJokes.Error in
+                    switch error {
+                    case is URLError:
+                        return .network
+                    case is DecodingError:
+                        return .parsing
+                    default:
+                        return error as? DadJokes.Error ?? .unknown
+                    }
+                })
+            .eraseToAnyPublisher()
+        }
+    }
+    
+    let api = DadJokes()
+    let jokeID = "9prWnjyImyd"
+    let badJokeID = "123456"
+    
+    api
+        .getJoke(id: jokeID)
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print("Go to joke: \($0)") })
+        .store(in: &subscriptions)
+}
 //: [Next](@next)
 
 /// Copyright (c) 2021 Razeware LLC
