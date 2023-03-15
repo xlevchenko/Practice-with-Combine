b969c163ee440711488279df96f12bd3fa2da377
Olexsii <realcarter50@gmail.com>
2023-03-15 22:14:11 +0200

    Schedule Chapter 17 Final

--- Schedulers.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
+++ Schedulers.playground/Pages/DispatchQueue.xcplaygroundpage/Contents.swift
@@ -2,36 +2,23 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
-<# Add code here #>
+let serialQueue = DispatchQueue(label: "Serial queue")
+let sourceQueue = serialQueue
 
-//: [Next](@next)
-/*:
- Copyright (c) 2021 Razeware LLC
+let source = PassthroughSubject<Void, Never>()
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+let subscription = sourceQueue.schedule(after: sourceQueue.now, interval: .seconds(1)) {
+    source.send()
+}
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+let setupPublisher = { recorder in
+    source
+        .recordThread(using: recorder)
+        .receive(on: serialQueue, options: DispatchQueue.SchedulerOptions(qos: .userInteractive))
+        .recordThread(using: recorder)
+        .eraseToAnyPublisher()
+}
 
- Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- distribute, sublicense, create a derivative work, and/or sell copies of the
- Software in any work that is designed, intended, or marketed for pedagogical or
- instructional purposes related to programming, coding, application development,
- or information technology.  Permission for such use, copying, modification,
- merger, publication, distribution, sublicensing, creation of derivative works,
- or sale is expressly withheld.
+let view = ThreadRecorderView(title: "Using DispatchQueue", setup: setupPublisher)
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
-
+PlaygroundPage.current.liveView = UIHostingController(rootView: view)
--- Schedulers.playground/Pages/RunLoop.xcplaygroundpage/Contents.swift
+++ Schedulers.playground/Pages/RunLoop.xcplaygroundpage/Contents.swift
@@ -2,6 +2,8 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
+var threadRecorder: ThreadRecorder? = nil
+
 let source = Timer
   .publish(every: 1.0, on: .main, in: .common)
   .autoconnect()
@@ -9,12 +11,22 @@ let source = Timer
 
 let setupPubliser = { recorder in
    source
-        .receive(on: DispatchQueue.global())
+        .subscribe(on: DispatchQueue.global())
+    //.receive(on: DispatchQueue.global())
         .recordThread(using: recorder)
         .receive(on: RunLoop.current)
         .recordThread(using: recorder)
+        .handleEvents(receiveSubscription: { _ in
+            threadRecorder = recorder
+        })
         .eraseToAnyPublisher()
 }
 
+
+RunLoop.current.schedule(
+    after: .init(Date(timeIntervalSinceNow: 4.5)),
+    tolerance: .milliseconds(500)) {
+        threadRecorder?.subscription?.cancel()
+}
 let view = ThreadRecorderView(title: "Using RunLoop", setup: setupPubliser)
 PlaygroundPage.current.liveView = UIHostingController(rootView: view)
