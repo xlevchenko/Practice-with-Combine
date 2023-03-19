cd5dfeaea054ff80b6d9e71213c25b5c7811e5b4
Olexsii <realcarter50@gmail.com>
2023-03-19 22:46:01 +0200

    Create custom timer operator

--- Custom Publisher.playground/Pages/DispatchTimer publisher.xcplaygroundpage/Contents.swift
+++ Custom Publisher.playground/Pages/DispatchTimer publisher.xcplaygroundpage/Contents.swift
@@ -1,35 +1,63 @@
 import Foundation
 import Combine
 
-<#Add your code here#>
+struct DispatchTimerConfiguration {
+    
+    let queue: DispatchQueue?
+    
+    let interval: DispatchTimeInterval
+    
+    let leeway: DispatchTimeInterval
+    
+    let times: Subscribers.Demand
+}
 
-//: [Next](@next)
-/*:
- Copyright (c) 2021 Razeware LLC
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
-
- Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- distribute, sublicense, create a derivative work, and/or sell copies of the
- Software in any work that is designed, intended, or marketed for pedagogical or
- instructional purposes related to programming, coding, application development,
- or information technology.  Permission for such use, copying, modification,
- merger, publication, distribution, sublicensing, creation of derivative works,
- or sale is expressly withheld.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+extension Publishers {
+    
+    struct DispatchTimer: Publisher {
+        
+        typealias Output = DispatchTime
+        typealias Failure = Never
+        
+        let configuration: DispatchTimerConfiguration
+        
+        init(configuration: DispatchTimerConfiguration) {
+            self.configuration = configuration
+        }
+        
+        func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, DispatchTime == S.Input {
+            let subscription = DispatchTimerSubscription(subscriber: subscriber, configuration: configuration)
+            
+            subscriber.receive(subscription: subscription)
+        }
+    }
+    
+    private final class DispatchTimerSubscription<S: Subscriber>: Subscription where S.Input == DispatchTimer {
+        
+        let configuration: DispatchTimerConfiguration
+        
+        let times: Subscribers.Demand
+        
+        var requested: Subscribers.Demand = .none
+        
+        var source: DispatchSourceTimer? = nil
+        
+        var subscriber: S?
+        
+        init(subscriber: S, configuration: DispatchTimerConfiguration) {
+            self.configuration = configuration
+            self.subscriber = subscriber
+            self.times = configuration.times
+        }
+        
+        func request(_ demand: Subscribers.Demand) {
+            <#code#>
+        }
+        
+        func cancel() {
+            source = nil
+            subscriber = nil
+        }
+    }
+}
