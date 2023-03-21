1c565ef8e5ae15c70b2bc0d25630d0bc4b64b204
Olexsii <realcarter50@gmail.com>
2023-03-21 18:25:45 +0200

    DispatchTimer publisher

--- Custom Publisher.playground/Pages/DispatchTimer publisher.xcplaygroundpage/Contents.swift
+++ Custom Publisher.playground/Pages/DispatchTimer publisher.xcplaygroundpage/Contents.swift
@@ -2,7 +2,6 @@ import Foundation
 import Combine
 
 struct DispatchTimerConfiguration {
-    
     let queue: DispatchQueue?
     
     let interval: DispatchTimeInterval
@@ -26,38 +25,89 @@ extension Publishers {
             self.configuration = configuration
         }
         
-        func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, DispatchTime == S.Input {
+        func receive<S: Subscriber>(subscriber: S) where Failure == S.Failure, Output == S.Input {
             let subscription = DispatchTimerSubscription(subscriber: subscriber, configuration: configuration)
             
             subscriber.receive(subscription: subscription)
         }
     }
+}
+
+private final class DispatchTimerSubscription<S: Subscriber>: Subscription where S.Input == DispatchTime {
     
-    private final class DispatchTimerSubscription<S: Subscriber>: Subscription where S.Input == DispatchTimer {
-        
-        let configuration: DispatchTimerConfiguration
-        
-        let times: Subscribers.Demand
-        
-        var requested: Subscribers.Demand = .none
-        
-        var source: DispatchSourceTimer? = nil
-        
-        var subscriber: S?
-        
-        init(subscriber: S, configuration: DispatchTimerConfiguration) {
-            self.configuration = configuration
-            self.subscriber = subscriber
-            self.times = configuration.times
+    let configuration: DispatchTimerConfiguration
+    
+    var times: Subscribers.Demand
+    
+    var requested: Subscribers.Demand = .none
+    
+    var source: DispatchSourceTimer? = nil
+    
+    var subscriber: S?
+    
+    init(subscriber: S, configuration: DispatchTimerConfiguration) {
+        self.configuration = configuration
+        self.subscriber = subscriber
+        self.times = configuration.times
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        guard times >  .none else {
+            subscriber?.receive(completion: .finished)
+            return
         }
         
-        func request(_ demand: Subscribers.Demand) {
-            <#code#>
-        }
+        requested += demand
         
-        func cancel() {
-            source = nil
-            subscriber = nil
+        if source == nil, requested > .none {
+            let source = DispatchSource.makeTimerSource(queue: configuration.queue)
+            
+            source.schedule(deadline: .now() + configuration.interval,
+                            repeating: configuration.interval,
+                            leeway: configuration.leeway)
+            
+            source.setEventHandler { [weak self] in
+                
+                guard let self = self,
+                      self.requested > .none else { return }
+                self.requested -= .max(1)
+                self.times -= .max(1)
+                
+                _ = self.subscriber?.receive(.now())
+                
+                if self.times == .none {
+                    self.subscriber?.receive(completion: .finished)
+                }
+            }
+            self.source = source
+            source.activate()
         }
     }
+    
+    func cancel() {
+        source = nil
+        subscriber = nil
+    }
+}
+
+
+extension Publishers {
+    static func timer(queue: DispatchQueue? = nil,
+                      interval: DispatchTimeInterval,
+                      leeway: DispatchTimeInterval = .nanoseconds(0),
+                      times: Subscribers.Demand = .unlimited) -> Publishers.DispatchTimer {
+        return Publishers.DispatchTimer(configuration: .init(queue: queue, interval: interval, leeway: leeway, times: times))
+    }
+}
+
+var logger = TimeLogger(sinceOrigin: true)
+
+let publisher = Publishers.timer(interval: .seconds(1), times: .max(6))
+
+let subscription = publisher.sink { time in
+    print("Timer emits: \(time)", to: &logger)
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+    subscription.cancel()
 }
