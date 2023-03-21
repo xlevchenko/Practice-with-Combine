7c81e0fe736cb6ce932575cc6eb67f929bd3902d
Olexsii <realcarter50@gmail.com>
2023-03-21 19:05:45 +0200

    Create new custom publisher

--- Custom Publisher.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
+++ Custom Publisher.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
@@ -1,4 +1,11 @@
 import Foundation
 import Combine
 
-<#Add your code here#>
+private final class ShareReplaySubscription<Output, Failure: Error>: Subscription {
+    
+    let copacity: Int
+    var subscriber: AnySubscriber<Output, Failure>? = nil
+    var demand: Subscribers.Demand = .none
+    var buffer: [Output]
+    var complition: Subscribers.Completion<Failure>? = nil
+}
