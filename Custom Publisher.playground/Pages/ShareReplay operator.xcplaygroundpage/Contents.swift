354b5ebb52512cdbad2da42c32cd070603ade6d9
Olexsii <realcarter50@gmail.com>
2023-03-24 16:39:59 +0200

    Add init for class

--- Custom Publisher.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
+++ Custom Publisher.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
@@ -3,9 +3,29 @@ import Combine
 
 private final class ShareReplaySubscription<Output, Failure: Error>: Subscription {
     
-    let copacity: Int
+    let capacity: Int
     var subscriber: AnySubscriber<Output, Failure>? = nil
     var demand: Subscribers.Demand = .none
     var buffer: [Output]
-    var complition: Subscribers.Completion<Failure>? = nil
+    var completion: Subscribers.Completion<Failure>? = nil
+    
+    init<S>(subscriber: S,
+            replay: [Output],
+            capacity: Int,
+            completion: Subscribers.Completion<Failure>?)
+    where S: Subscriber, Failure == S.Failure, Output == S.Input  {
+        
+        self.subscriber = AnySubscriber(subscriber)
+        self.buffer = replay
+        self.capacity = capacity
+        self.completion = completion
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        
+    }
+    
+    func cancel() {
+        
+    }
 }
