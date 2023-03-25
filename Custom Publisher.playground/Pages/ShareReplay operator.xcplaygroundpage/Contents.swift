85124f0d7a0bf37d25ffe7d915cd35c69be68f79
Olexsii <realcarter50@gmail.com>
2023-03-25 23:48:17 +0200

    func receive

--- Custom Publisher.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
+++ Custom Publisher.playground/Pages/ShareReplay operator.xcplaygroundpage/Contents.swift
@@ -21,11 +21,62 @@ private final class ShareReplaySubscription<Output, Failure: Error>: Subscriptio
         self.completion = completion
     }
     
-    func request(_ demand: Subscribers.Demand) {
+    private func complete(with completion: Subscribers.Completion<Failure>) {
         
+        guard let subscriber = subscriber else { return }
+        
+        self.subscriber = nil
+        self.completion = nil
+        self.buffer.removeAll()
+        
+        subscriber.receive(completion: completion)
+    }
+    
+    private func emmitAsNeded() {
+        guard let subscriber = subscriber else { return }
+        
+        while self.demand > .none && !buffer.isEmpty {
+            self.demand = .max(1)
+            
+            let nextDemand = subscriber.receive(buffer.removeFirst())
+            
+            if nextDemand != .none {
+                self.demand += nextDemand
+            }
+        }
+        
+        if let completion = completion {
+            complete(with: completion)
+        }
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        if demand != .none {
+            self.demand += demand
+        }
+        emmitAsNeded()
     }
     
     func cancel() {
+        complete(with: .finished)
+    }
+    
+    func receive(_ input: Output) {
+        guard subscriber != nil else { return }
         
+        buffer.append(input)
+        
+        if buffer.count > capacity {
+            buffer.removeFirst()
+        }
+        emmitAsNeded()
+    }
+    
+    func receive(completion: Subscribers.Completion<Failure>) {
+        guard let subscriber = subscriber else { return }
+        
+        self.subscriber = nil
+        self.buffer.removeAll()
+        subscriber.receive(completion: completion)
     }
 }
