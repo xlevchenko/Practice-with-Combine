ca93f6652b36a763ca375683539b7210ab6944ad
Olexsii <realcarter50@gmail.com>
2023-04-17 14:30:16 +0300

    Extending Joke Managed Object to save jokes

---
+++ ChuckNorrisJokes/ChuckNorrisJokes/Models/JokeManagedObject.swift
@@ -0,0 +1,39 @@
+//
+//  JokeManagedObject.swift
+//  ChuckNorrisJokes
+//
+//  Created by Olexsii Levchenko on 4/17/23.
+//  Copyright © 2023 Scott Gardner. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import CoreData
+import ChuckNorrisJokesModel
+
+
+extension JokeManagedObject {
+    static func seve(joke: Joke, inViewContext viewContext: NSManagedObjectContext) {
+        guard joke.id != "error" else { return }
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: JokeManagedObject.self))
+        fetchRequest.predicate = NSPredicate(format: "id = %@", joke.id)
+        
+        if let result = try? viewContext.fetch(fetchRequest),
+           let existing = result.first as? JokeManagedObject {
+            existing.value = joke.value
+            existing.categories = joke.categories as NSArray
+        } else {
+            let newJoke = self.init(context: viewContext)
+            newJoke.id = joke.id
+            newJoke.value = joke.value
+            newJoke.categories = joke.categories as NSArray
+        }
+        
+        do {
+            try viewContext.save()
+        } catch {
+            fatalError("\(#file), \(#function), \(error.localizedDescription)")
+        }
+    }
+}
